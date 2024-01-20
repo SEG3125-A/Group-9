9a4048942015f328f1183929ae88e3c59e32be71
Kanjanokphat Kitisuwanakul <77587871+AmyKKiti@users.noreply.github.com>
2024-01-20 13:20:33 -0500

    Improve style

--- Lab2/scripts/cart.js
+++ Lab2/scripts/cart.js
@@ -3,50 +3,83 @@ function displayCart(){
     var cartPage = document.getElementById("cart-page");
     var emptyCart = true;
 
-    cartContainer.innerHTML = ''; //clear html
+    //clear html
+    cartContainer.innerHTML = '';
     
     for(i = 0; i < products.length; i++){
         console.log(products[i].name + " " + quantities[i]);
 
         if (quantities[i] != 0){
             //set the cart items
-            cartItemDiv = document.createElement('div');
-            cartItemDiv.classList.add('cart-item-container');
+    
+            //div for each item at checkout
+            var checkoutItem = document.createElement('div');
+            checkoutItem.className = 'cart-checkout-item';
 
-            cartItemDiv.innerHTML = `
-            <p>${products[i].name}</p>
-            <p>x${quantities[i]}</p>`
+            //div for product name
+            var productName = document.createElement('div');
+            productName.textContent = products[i].name;
 
-            cartContainer.appendChild(cartItemDiv);
-            emptyCart = false;
+            // Create a div for the quantity
+            var quantity = document.createElement('div');
+            quantity.className = 'cart-quantity';
+            quantity.textContent = 'x ' + quantities[i];
+
+            //build checkout item
+            checkoutItem.appendChild(productName);
+            checkoutItem.appendChild(quantity);
+
+            //append to cart container
+            cartContainer.appendChild(checkoutItem);
+
+            emptyCart = false; //there are items in the cart
         }
     }
+
+    cartContainer.appendChild(document.createElement('br'));
     
     if (emptyCart){
         cartContainer.innerHTML = "Shopping cart is empty.";
     }else{
         //shopping cart is not empty, display total price
         totalCostDiv = document.createElement('div');
-        totalCostDiv.classList.add('cart-item-container');
+        totalCostDiv.className = 'total-checkout';
 
-        const total = calculateTotalCost();
-        totalCostDiv = document.createElement('div');
-        totalCostDiv.classList.add('cart-item-container')
+        //div for total text
+        var totalText = document.createElement('div');
+        totalText.textContent = "Total Cost:";
+        totalText.style.fontWeight = 'bold';
 
-        cartContainer.appendChild(document.createElement("HR"));
-        totalCostDiv.innerHTML = `
-            <p style="font-weight:bold">Total Cost: </p>
-            <p style="font-weight:bold"> $ ${total}</p>`
+        //div for total cost
+        const sum = calculateTotalCost();
+        totalCost = document.createElement('div');
+        totalCost.className = 'cart-quantity';
+        totalCost.textContent = '$ ' + sum;
+        totalCost.style.fontWeight = 'bold';
 
+        totalCostDiv.appendChild(totalText);
+        totalCostDiv.appendChild(totalCost);
         cartContainer.appendChild(totalCostDiv);    
         
-        //TODO: style the pay button
-        checkout = document.createElement('BUTTON');
-        var t = document.createTextNode("Pay");
-        checkout.appendChild(t);
-        cartContainer.appendChild(checkout);
+        //optional pay button
+        payContainer = document.createElement('div');
+        payContainer.className = 'pay-container';
+        
+        checkoutBtn = document.createElement('BUTTON');
+        var text = document.createTextNode("Pay");
+
+        //style
+        checkoutBtn.style.backgroundColor = '#282c34';
+        checkoutBtn.style.color = 'white';
+        checkoutBtn.style.border = 'none';
+        checkoutBtn.style.borderRadius = '10px';
+        checkoutBtn.style.cursor = 'pointer';
+        checkoutBtn.style.padding = '5px 20px 5px 20px';
+
+        checkoutBtn.appendChild(text);
+        payContainer.appendChild(checkoutBtn);
+        cartContainer.appendChild(payContainer);
     }
-    
 }
 
 function calculateTotalCost(){
