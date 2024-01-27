ef2e8e7ee1cc4f66a4536d8b9ce322252127d18f
Celine Wan <mwanm074@uottawa.ca>
2024-01-27 15:37:03 -0500

    Added full implementation for checkout page
    Also added placeholders for input
    Updated some styling

--- Lab3/scripts/cart.js
+++ Lab3/scripts/cart.js
@@ -1,4 +1,11 @@
 var emptyCart = true;
+var card = '';
+var firstName = '';
+var lastName = '';
+var street = '';
+var city = '';
+var province = '';
+var zipCode = '';
 
 function displayCart() {
   // Switches to cart page
@@ -7,6 +14,12 @@ function displayCart() {
   var cartContainer = document.getElementById("cart-container");
 
   cartContainer.innerHTML = "";
+
+  var header = document.createElement("h3");
+  var cartHeader = document.createTextNode("Shopping Cart");
+
+  header.appendChild(cartHeader);
+  cartContainer.appendChild(header);
 
   for (i = 0; i < products.length; i++) {
     if (quantities[i] != 0) {
@@ -44,18 +57,18 @@ function displayCart() {
   } else {
     //shopping cart is not empty, display total price
     totalCostDiv = document.createElement("div");
-    totalCostDiv.className = "total-checkout";
+    totalCostDiv.className = "total-checkout-line";
 
     //div for total text
     var totalText = document.createElement("div");
-    totalText.textContent = "Total Cost:";
+    totalText.textContent = "Subtotal:";
     totalText.className = "bold-text";
 
     //div for total cost
-    const sum = calculateTotalCost();
+    const sum = calculateSubtotalCost();
     totalCost = document.createElement("div");
     totalCost.className = "bold-text";
-    totalCost.textContent = "$ " + sum;
+    totalCost.textContent = "$" + sum;
 
     totalCostDiv.appendChild(totalText);
     totalCostDiv.appendChild(totalCost);
@@ -65,23 +78,23 @@ function displayCart() {
     payContainer = document.createElement("div");
     payContainer.className = "pay-container";
 
-    checkoutBtn = document.createElement("BUTTON");
-    checkoutBtn.className = "checkout-btn hover-btn";
-    var text = document.createTextNode("Pay");
+    checkoutBtn = document.createElement("button");
+    checkoutBtn.className = "checkout-btn";
+    var text = document.createTextNode("Proceed to checkout");
     checkoutBtn.appendChild(text);
     checkoutBtn.addEventListener("click", pay);
 
     payContainer.appendChild(checkoutBtn);
     cartContainer.appendChild(payContainer);
 
-    if(sum == 0){
+    if (sum == 0) {
       emptyCart = true;
       cartContainer.innerHTML = "Shopping cart is empty.";
     }
   }
 }
 
-function calculateTotalCost() {
+function calculateSubtotalCost() {
   var totalCost = 0;
   for (i = 0; i < products.length; i++) {
     totalCost = totalCost + products[i].unitPrice * quantities[i];
@@ -91,12 +104,12 @@ function calculateTotalCost() {
 
 function changeCartTab(event, tabName) {
   // If the cart is empty don't let the tab change
-  if(emptyCart) return;
+  if (emptyCart) return;
 
   let cart = document.getElementById("cart-container");
   let billing = document.getElementById("billing-container");
   let shipping = document.getElementById("shipping-container");
-  let payment = document.getElementById("payment-container");
+  let payment = document.getElementById("checkout-container");
 
   let tabs = {
     cart: cart,
@@ -147,15 +160,13 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    var name = document.getElementById("name").value;
-    var address = document.getElementById("address").value;
-    var card = document.getElementById("card").value;
+    card = document.getElementById("card").value;
   });
 
 function goToShipping() {
-  var name = document.getElementById("name").value;
-  var address = document.getElementById("address").value;
-  var card = document.getElementById("card").value;
+  name = document.getElementById("name").value;
+  address = document.getElementById("address").value;
+  card = document.getElementById("card").value;
 
   if (name && address && card) {
     document.getElementById("billing-feedback").textContent =
@@ -171,6 +182,189 @@ function goToShipping() {
   }
 }
 
-function gotoPayment(){
-    clickButton("payment-button");
+function gotoPayment() {
+  firstName = document.getElementById('fname').value;
+  lastName = document.getElementById('lname').value;
+  street = document.getElementById('street').value;
+  city = document.getElementById('city').value;
+  province = document.getElementById('province').value;
+  zipCode = document.getElementById('zip').value;
+  clickButton("payment-button");
+  displayCheckout();
+}
+
+function getShippingInfo() {
+
+  var shippingInfoDiv = document.createElement("div");
+  shippingInfoDiv.className = "shipping-summary";
+
+  var fullNameLine = document.createElement("p");
+  var addressLine1 = document.createElement("p");
+  var addressLine2 = document.createElement("p");
+
+  var fullName = document.createTextNode(`${firstName} ${lastName}`);
+  var address1 = document.createTextNode(street);
+  var address2 = document.createTextNode(`${city}, ${province} ${zipCode}`);
+
+  fullNameLine.appendChild(fullName);
+  addressLine1.appendChild(address1);
+  addressLine2.appendChild(address2);
+
+  shippingInfoDiv.appendChild(fullNameLine);
+  shippingInfoDiv.appendChild(addressLine1);
+  shippingInfoDiv.appendChild(addressLine2);
+
+  console.log(shippingInfoDiv);
+  return shippingInfoDiv;
+
+}
+
+function getCardInfo() {
+  var paymentMethodDiv = document.createElement("div");
+  paymentMethodDiv.style.lineHeight = '5px';
+  var paymentMethodLine = document.createElement("p");
+  console.log(typeof card);
+  console.log(card);
+  console.log(card.trim().slice(-4));
+  var cardInfoEnd = document.createTextNode(`Paying with card ending in ${card.slice(-4)}`);
+
+  paymentMethodLine.appendChild(cardInfoEnd);
+  paymentMethodDiv.appendChild(paymentMethodLine);
+
+  return paymentMethodDiv;
+}
+
+function displayCheckout() {
+  var checkoutContainer = document.getElementById("checkout-container");
+
+  checkoutContainer.innerHTML = "<h3>Checkout<h3><hr>";
+
+  var shippingInfoHeader = document.createElement("h4");
+  var shippingInfoTitle = document.createTextNode("Shipping address");
+  shippingInfoHeader.appendChild(shippingInfoTitle);
+  checkoutContainer.appendChild(shippingInfoHeader);
+
+  const shippingInfoDiv = getShippingInfo();
+  checkoutContainer.appendChild(shippingInfoDiv);
+
+  var paymentDiv = document.createElement("h4");
+  var paymentTitle = document.createTextNode("Payment method");
+  paymentDiv.appendChild(paymentTitle);
+  checkoutContainer.appendChild(paymentDiv);
+
+  var paymentMethodInfoDiv = getCardInfo();
+  checkoutContainer.appendChild(paymentMethodInfoDiv);
+
+  var orderDiv = document.createElement("h4");
+  var orderTitle = document.createTextNode("Order Summary");
+  orderDiv.appendChild(orderTitle);
+  checkoutContainer.appendChild(orderDiv);
+
+  for (i = 0; i < products.length; i++) {
+    if (quantities[i] != 0) {
+
+      var checkoutItem = document.createElement("div");
+      checkoutItem.className = "cart-checkout-item";
+
+      var productName = document.createElement("div");
+      productName.textContent = products[i].name;
+
+      var quantity = document.createElement("div");
+      quantity.className = "bold-text";
+      quantity.textContent =
+        "$" + products[i].unitPrice + " (x" + quantities[i] + ")";
+
+      checkoutItem.appendChild(productName);
+      checkoutItem.appendChild(quantity);
+
+      checkoutContainer.appendChild(checkoutItem);
+    }
+  }
+
+  checkoutContainer.appendChild(document.createElement("br"));
+
+  var orderTotalDiv = document.createElement("div");
+
+  var subtotalDiv = document.createElement("div");
+  subtotalDiv.className = "total-checkout-line";
+
+  var subtotalText = document.createElement("div");
+  subtotalText.textContent = "Subtotal:";
+  subtotalText.className = "bold-text";
+
+  const subtotal = calculateSubtotalCost();
+  var subtotalAmount = document.createElement("div");
+  subtotalAmount.className = "bold-text";
+  subtotalAmount.textContent = "$" + subtotal;
+
+  subtotalDiv.appendChild(subtotalText);
+  subtotalDiv.appendChild(subtotalAmount);
+
+  const tax = getEstimatedTax(subtotal);
+
+  var taxDiv = document.createElement("div");
+  taxDiv.className = "total-checkout-line";
+
+  var taxText = document.createElement("div");
+  taxText.textContent = "Estimated GST/HST:";
+
+  var taxAmount = document.createElement("div");
+  taxAmount.textContent = "$" + tax;
+
+  taxDiv.appendChild(taxText);
+  taxDiv.appendChild(taxAmount);
+
+  orderTotalDiv.appendChild(subtotalDiv)
+  orderTotalDiv.appendChild(taxDiv);
+
+  orderTotalDiv.appendChild(document.createElement("hr"));
+
+  const orderTotal = getOrderTotal(subtotal, tax);
+
+  var totalDiv = document.createElement("div");
+  totalDiv.className = "total-checkout-line";
+
+  var totalText = document.createElement("div");
+  totalText.textContent = "Order Total:";
+  totalText.className = "bold-text";
+
+  var totalAmount = document.createElement("div");
+  totalAmount.textContent = "$" + orderTotal;
+  totalAmount.className = "bold-text";
+
+  totalDiv.appendChild(totalText);
+  totalDiv.appendChild(totalAmount);
+
+  orderTotalDiv.appendChild(totalDiv);
+  checkoutContainer.appendChild(orderTotalDiv);
+
+  var checkoutButtonContainer = document.createElement("div");
+  checkoutButtonContainer.style.textAlign = "right";
+  checkoutButtonContainer.style.marginTop = "10px";
+  var checkoutButton = document.createElement("button");
+  checkoutButton.className = "checkout-btn";
+  var buttonText = document.createTextNode("Place order");
+  checkoutButton.onclick = placeOrder;
+  checkoutButton.appendChild(buttonText);
+  checkoutButtonContainer.appendChild(checkoutButton);
+  checkoutContainer.appendChild(checkoutButtonContainer);
+}
+
+function getEstimatedTax(subtotal) {
+  const tax = subtotal * (0.13);
+  return tax.toFixed(2);
+}
+
+function getOrderTotal(subtotal, tax) {
+  const orderTotal = parseFloat(subtotal) + parseFloat(tax);
+  return orderTotal.toFixed(2);
+}
+
+function placeOrder() {
+  var paymentMessage = document.getElementById("payment-message");
+  paymentMessage.style.display = "flex";
+}
+
+function closePaymentMessage() {
+  document.getElementById("payment-message").style.display = "none";
 }
