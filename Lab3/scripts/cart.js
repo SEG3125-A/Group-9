abddfff7efeccbad3ed75cc066199516c3aef28b
Amar Ghazi <80402304+amarghazi@users.noreply.github.com>
2024-01-26 19:12:30 -0500

    Implement billing page and styling it

--- Lab3/scripts/cart.js
+++ Lab3/scripts/cart.js
@@ -1,133 +1,163 @@
-function displayCart(){
-    
-    // Switches to cart page
-    clickButton("cart-button");
+function displayCart() {
+  // Switches to cart page
+  clickButton("cart-button");
 
-    var cartContainer = document.getElementById("cart-container");
-    var emptyCart = true;
+  var cartContainer = document.getElementById("cart-container");
+  var emptyCart = true;
 
-    cartContainer.innerHTML = ""
+  cartContainer.innerHTML = "";
 
-    for(i = 0; i < products.length; i++){
-        if (quantities[i] != 0){
-            //set the cart items
-    
-            //div for each item at checkout
-            var checkoutItem = document.createElement('div');
-            checkoutItem.className = 'cart-checkout-item';
+  for (i = 0; i < products.length; i++) {
+    if (quantities[i] != 0) {
+      //set the cart items
 
-            //div for product name
-            var productName = document.createElement('div');
-            productName.textContent = products[i].name;
+      //div for each item at checkout
+      var checkoutItem = document.createElement("div");
+      checkoutItem.className = "cart-checkout-item";
 
-            // Create a div for the quantity
-            var quantity = document.createElement('div');
-            quantity.className = 'bold-text';
-            quantity.textContent = '$' + products[i].unitPrice + ' (x' + quantities[i] + ')';
+      //div for product name
+      var productName = document.createElement("div");
+      productName.textContent = products[i].name;
 
-            //build checkout item
-            checkoutItem.appendChild(productName);
-            checkoutItem.appendChild(quantity);
+      // Create a div for the quantity
+      var quantity = document.createElement("div");
+      quantity.className = "bold-text";
+      quantity.textContent =
+        "$" + products[i].unitPrice + " (x" + quantities[i] + ")";
 
-            //append to cart container
-            cartContainer.appendChild(checkoutItem);
+      //build checkout item
+      checkoutItem.appendChild(productName);
+      checkoutItem.appendChild(quantity);
 
-            emptyCart = false; //there are items in the cart
-        }
+      //append to cart container
+      cartContainer.appendChild(checkoutItem);
+
+      emptyCart = false; //there are items in the cart
     }
+  }
 
-    cartContainer.appendChild(document.createElement('br'));
-    
-    if (emptyCart){
-        cartContainer.innerHTML = "Shopping cart is empty.";
-    }else{
-        //shopping cart is not empty, display total price
-        totalCostDiv = document.createElement('div');
-        totalCostDiv.className = 'total-checkout';
+  cartContainer.appendChild(document.createElement("br"));
 
-        //div for total text
-        var totalText = document.createElement('div');
-        totalText.textContent = "Total Cost:";
-        totalText.className = 'bold-text';
+  if (emptyCart) {
+    cartContainer.innerHTML = "Shopping cart is empty.";
+  } else {
+    //shopping cart is not empty, display total price
+    totalCostDiv = document.createElement("div");
+    totalCostDiv.className = "total-checkout";
 
-        //div for total cost
-        const sum = calculateTotalCost();
-        totalCost = document.createElement('div');
-        totalCost.className = 'bold-text';
-        totalCost.textContent = '$ ' + sum;
+    //div for total text
+    var totalText = document.createElement("div");
+    totalText.textContent = "Total Cost:";
+    totalText.className = "bold-text";
 
-        totalCostDiv.appendChild(totalText);
-        totalCostDiv.appendChild(totalCost);
-        cartContainer.appendChild(totalCostDiv);    
-        
-        //optional pay button
-        payContainer = document.createElement('div');
-        payContainer.className = 'pay-container';
-        
-        checkoutBtn = document.createElement('BUTTON');
-        checkoutBtn.className = 'checkout-btn hover-btn';
-        var text = document.createTextNode("Pay");
-        checkoutBtn.appendChild(text);
-        checkoutBtn.addEventListener("click", pay);
+    //div for total cost
+    const sum = calculateTotalCost();
+    totalCost = document.createElement("div");
+    totalCost.className = "bold-text";
+    totalCost.textContent = "$ " + sum;
 
-        payContainer.appendChild(checkoutBtn);
-        cartContainer.appendChild(payContainer);
-    }
+    totalCostDiv.appendChild(totalText);
+    totalCostDiv.appendChild(totalCost);
+    cartContainer.appendChild(totalCostDiv);
+
+    //optional pay button
+    payContainer = document.createElement("div");
+    payContainer.className = "pay-container";
+
+    checkoutBtn = document.createElement("BUTTON");
+    checkoutBtn.className = "checkout-btn hover-btn";
+    var text = document.createTextNode("Pay");
+    checkoutBtn.appendChild(text);
+    checkoutBtn.addEventListener("click", pay);
+
+    payContainer.appendChild(checkoutBtn);
+    cartContainer.appendChild(payContainer);
+  }
 }
 
-function calculateTotalCost(){
-    var totalCost = 0;
-    for(i = 0; i < products.length; i++){
-        totalCost = totalCost + (products[i].unitPrice * quantities[i]);
-    }
-    return totalCost.toFixed(2);
-} 
-
-
+function calculateTotalCost() {
+  var totalCost = 0;
+  for (i = 0; i < products.length; i++) {
+    totalCost = totalCost + products[i].unitPrice * quantities[i];
+  }
+  return totalCost.toFixed(2);
+}
 
 function changeCartTab(event, tabName) {
-    let cart = document.getElementById("cart-container");
-    let billing = document.getElementById("billing-container");
-    let shipping = document.getElementById("shipping-container");
-    let payment = document.getElementById("payment-container");
+  let cart = document.getElementById("cart-container");
+  let billing = document.getElementById("billing-container");
+  let shipping = document.getElementById("shipping-container");
+  let payment = document.getElementById("payment-container");
 
-    let tabs = {"cart": cart, "billing": billing, "shipping": shipping, "payment": payment};
-    // Array that only contains document elements
-    let tabValues = Object.values(tabs);
+  let tabs = {
+    cart: cart,
+    billing: billing,
+    shipping: shipping,
+    payment: payment,
+  };
+  // Array that only contains document elements
+  let tabValues = Object.values(tabs);
 
-    // used code from lab to make buttons active
-    buttonsNav = document.getElementsByClassName("btn-nav-cart");
-    for (i = 0; i < buttonsNav.length; i++) {
-      buttonsNav[i].className = buttonsNav[i].className.replace(" active", "");
-      tabValues[i].style.display = "none";
-    }
-
-    tabs[tabName].style.display = "block";
-    changeProgressBar(event);
-    event.currentTarget.className += " active";
-
+  // used code from lab to make buttons active
+  buttonsNav = document.getElementsByClassName("btn-nav-cart");
+  for (i = 0; i < buttonsNav.length; i++) {
+    buttonsNav[i].className = buttonsNav[i].className.replace(" active", "");
+    tabValues[i].style.display = "none";
   }
-  
+
+  tabs[tabName].style.display = "block";
+  changeProgressBar(event);
+  event.currentTarget.className += " active";
+}
 
 function changeProgressBar(event) {
-    const stepperItems = document.querySelectorAll('.stepper-item');
-    stepperItems.forEach(item => item.classList.remove('completed'));
-    const selectedButton = event.currentTarget;
-    const selectedTabIndex = Array.from(stepperItems).indexOf(selectedButton.parentNode);
-  
-    for (let i = 0; i <= selectedTabIndex-1; i++) {
-      stepperItems[i].classList.add('completed');
-    }
+  const stepperItems = document.querySelectorAll(".stepper-item");
+  stepperItems.forEach((item) => item.classList.remove("completed"));
+  const selectedButton = event.currentTarget;
+  const selectedTabIndex = Array.from(stepperItems).indexOf(
+    selectedButton.parentNode
+  );
+
+  for (let i = 0; i <= selectedTabIndex - 1; i++) {
+    stepperItems[i].classList.add("completed");
+  }
 }
 
 // Switches to billing page
-function pay(){
-    clickButton("billing-button");
+function pay() {
+  clickButton("billing-button");
 }
 
-
-function clickButton(buttonName){
-    const billingButton = document.getElementById(buttonName);
-    billingButton.click();
+function clickButton(buttonName) {
+  const billingButton = document.getElementById(buttonName);
+  billingButton.click();
 }
 
+document
+  .getElementById("billing-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var address = document.getElementById("address").value;
+    var card = document.getElementById("card").value;
+  });
+
+function goToShipping() {
+  var name = document.getElementById("name").value;
+  var address = document.getElementById("address").value;
+  var card = document.getElementById("card").value;
+
+  if (name && address && card) {
+    document.getElementById("billing-feedback").textContent =
+      "Billing information saved successfully.";
+    document.getElementById("billing-container").style.display = "none";
+    document.getElementById("billing-feedback").style.color = "green";
+    document.getElementById("shipping-container").style.display = "block";
+    clickButton("shipping-button");
+  } else {
+    document.getElementById("billing-feedback").textContent =
+      "Please fill out all fields in the billing form.";
+    document.getElementById("billing-feedback").style.color = "red";
+  }
+}
