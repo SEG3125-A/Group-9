0feb728d1789bfd8445fee7deb052aefcaef8a9a
Selin Kararmaz <selinkararmaz1@gmail.com>
2024-01-27 17:42:24 -0500

    Fix slider bug

--- Lab3/scripts/priceRangeSlider.js
+++ Lab3/scripts/priceRangeSlider.js
@@ -5,7 +5,7 @@ function listenPriceChange(){
     priceInput = document.querySelectorAll(".price-input input");
     let priceGap = 0;
     
-    previousEntries = [parseInt(priceInput[0].value), parseInt(priceInput[1].value)];
+    previousEntries = [0, 40];
     updateBlueBar(previousEntries[0], previousEntries[1], rangeInput);
 
 
@@ -13,25 +13,25 @@ function listenPriceChange(){
         input.addEventListener("input", e =>{
             let minPrice = parseInt(priceInput[0].value),
             maxPrice = parseInt(priceInput[1].value);
-            if(maxPrice < minPrice || minPrice > maxPrice){
-                priceInput[0].value = previousEntries[0];
-                priceInput[1].value = previousEntries[1];
-                return;
-            }
             
+
             if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
-                updateBlueBar(minVal, maxVal, rangeInput);
+                if(e.target.className === "input-min"){
+                    rangeInput[0].value = minPrice;
+                    range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+                }else{
+                    rangeInput[1].value = maxPrice;
+                    range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+                }
             }
-        displayProducts();
-        previousEntries = [priceInput[0].value, priceInput[1].value]
+            displayProducts();
+            previousEntries = [priceInput[0].value, priceInput[1].value]
         });
     });
-  
     rangeInput.forEach(input =>{
         input.addEventListener("input", e =>{
             let minVal = parseInt(rangeInput[0].value),
             maxVal = parseInt(rangeInput[1].value);
-  
             if((maxVal - minVal) < priceGap){
                 if(e.target.className === "range-min"){
                     rangeInput[0].value = maxVal - priceGap
@@ -41,7 +41,8 @@ function listenPriceChange(){
             }else{
                 priceInput[0].value = minVal;
                 priceInput[1].value = maxVal;
-                updateBlueBar(minVal, maxVal, rangeInput);
+                range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
+                range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
             }
             displayProducts();
         });
