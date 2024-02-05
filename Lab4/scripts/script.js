b3ec092da1ac2312289c2e5c33ca8184c02378b9
Selin Kararmaz <selinkararmaz1@gmail.com>
2024-02-04 19:17:25 -0500

    Add simplified loading icon

---
+++ Lab4/scripts/script.js
@@ -0,0 +1,27 @@
+function mapLoaded(){
+    var loadingIcon = document.querySelector(".loading-icon");
+    loadingIcon.style.display = "none";
+}
+
+function showModal(event) {
+    let form = $("#booking-form")[0];
+
+    //check required fields
+    let isFormValid = form.checkValidity();
+    if (!isFormValid) {
+      form.reportValidity();
+    } else {
+      //if all filled correctly, then show modal
+      event.preventDefault();
+      $("#success-modal").modal("show");
+    }
+  }
+
+function selectService(serviceName) {
+    // Update the dropdown
+    document.getElementById("serviceSelection").value = serviceName;
+    // Scroll to the booking section
+    document
+    .getElementById("booking-section")
+    .scrollIntoView({ behavior: "smooth" });
+}
