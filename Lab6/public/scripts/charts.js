c4d25c4478da1c0fea46fd92b7df8fbf0eceac57
Celine Wan <mwanm074@uottawa.ca>
2024-02-25 14:57:52 -0500

    Implemented new UI for analysis with charts
    Also added new endpoint to get the jsonData

---
+++ Lab6/public/scripts/charts.js
@@ -0,0 +1,272 @@
+fetch('/api/data')
+    .then(response => response.json())
+    .then(data => {
+        const config = {
+            displayModeBar: false
+        }
+
+        /// PIE CHART FOR PROVINCE ////
+        console.log(data)
+        const provinceData = data.province;
+        const provinceCodes = ['AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT'];
+        const provinceLabels = ['Alberta', 'British Columbia', 'Manitoba', 'New Brunswick', 'Newfoundland and Labrador', 'Nova Scotia', 'Northwest Territories', 'Nunavut', 'Ontario', 'Prince Edward Island', 'Quebec', 'Saskatchewan', 'Yukon'];
+        var provinceValues = [];
+        provinceCodes.map((code) => {
+            provinceValues.push(provinceData[code])
+        })
+
+        let totalNumberOfReplies = 0;
+        for (let i = 0; i < provinceValues.length; i++) {
+            totalNumberOfReplies += provinceValues[i];
+        }
+        console.log("total ", totalNumberOfReplies)
+        var introduction = document.getElementById("introduction");
+        if (totalNumberOfReplies == 0) {
+            introduction.innerHTML = `<p>No one has replied to the survey yet.Please wait to get some responses before doing an analysis</p>`;
+        } else if (totalNumberOfReplies == 1) {
+            introduction.innerHTML = `<p>Only 1 person has replied to the survey. Please find the results below to be able to do an analysis.</p>`;
+        } else {
+            introduction.innerHTML = `<p>${totalNumberOfReplies} people have replied to the survey. Please find the results below to be able to do an analysis.</p>`;
+        }
+
+        const provinceLayout = {
+            title: {
+                text: "Demographic of the users that participated in the survey (Province)",
+                font: {
+                    color: "white"
+                }
+            },
+            paper_bgcolor: '#282c34',
+            font: {
+                color: 'white'
+            },
+            hoverlabel: {
+                font: {
+                    color: 'white' // Set the font color of the tooltip
+                }
+            }
+        };
+
+        const provinceChart = [{
+            labels: provinceLabels,
+            values: provinceValues,
+            type: "pie",
+            textinfo: 'none'
+        }];
+
+        Plotly.newPlot("province-chart", provinceChart, provinceLayout, config);
+
+        /// BAR CHART FOR RATINGS ////
+        const ratingData = data.rating;
+        const ratingLabels = ["one", "two", "three", "four", "five"];
+        var ratingValues = [];
+        ratingLabels.map((rating) => {
+            ratingValues.push(ratingData[rating])
+        })
+
+        const ratingLayout = {
+            title: {
+                text: "UI Usability Rating (Out of 5)",
+                font: {
+                    color: "white"
+                }
+            },
+            plot_bgcolor: "#282c34",
+            paper_bgcolor: "#282c34",
+            xaxis: {
+                title: "Features",
+                titlefont: {
+                    color: 'white'
+                },
+                tickfont: {
+                    color: 'white'
+                },
+                linecolor: 'white',
+                zerolinecolor: 'white'
+            },
+            yaxis: {
+                title: "Number of votes received",
+                titlefont: {
+                    color: 'white'
+                },
+                tickfont: {
+                    color: 'white'
+                },
+                linecolor: 'white',
+            }
+        };
+
+        const ratingChart = [{
+            x: ratingLabels,
+            y: ratingValues,
+            type: "bar",
+            orientation: "v",
+            marker: { color: "#4682B4" },
+        }];
+
+        Plotly.newPlot("ratings-chart", ratingChart, ratingLayout, config);
+
+        /// BAR CHART FOR MOST USED FEATURES ////
+        const featuresData = data.features;
+        const featuresLabels = ["Sort and filter", "Flyers", "Store Finder", "Digital Services", "Trending in your area"];
+        var featureValues = [];
+        featuresLabels.map((feature) => {
+            featureValues.push(featuresData[feature])
+        })
+
+        const featureLayout = {
+            title: {
+                text: "Most used features",
+                font: {
+                    color: "white"
+                }
+            },
+            plot_bgcolor: "#282c34",
+            paper_bgcolor: "#282c34",
+            xaxis: {
+                title: "Features",
+                titlefont: {
+                    color: 'white'
+                },
+                tickfont: {
+                    color: 'white'
+                },
+                linecolor: 'white',
+                zerolinecolor: 'white'
+            },
+            yaxis: {
+                title: "Number of votes received",
+                titlefont: {
+                    color: 'white'
+                },
+                tickfont: {
+                    color: 'white'
+                },
+                linecolor: 'white',
+            }
+        };
+
+        const featureChart = [{
+            x: featuresLabels,
+            y: featureValues,
+            type: "bar",
+            orientation: "v",
+            marker: { color: "#4682B4" },
+        }];
+
+        Plotly.newPlot("features-chart", featureChart, featureLayout, config);
+
+        /// BAR CHART FOR MOST SHOPPED CATEGORY ////
+        const shopMostData = data.shopmost;
+        const shopMostLabels = ["Fruits and vegetables", "Dairy and Eggs", "Bread and Bakery", "Meat and Seafood", "Chips and Snacks", "Medicine and Drugs", "Other"];
+        var shopMostValues = [];
+        shopMostLabels.map((category) => {
+            shopMostValues.push(shopMostData[category])
+        })
+
+        const shopMostLayout = {
+            title: {
+                text: "Most Shopped Categories",
+                font: {
+                    color: "white"
+                }
+            },
+            plot_bgcolor: "#282c34",
+            paper_bgcolor: "#282c34",
+            xaxis: {
+                title: "Categories",
+                titlefont: {
+                    color: 'white'
+                },
+                tickfont: {
+                    color: 'white'
+                },
+                linecolor: 'white',
+                zerolinecolor: 'white'
+            },
+            yaxis: {
+                title: "Number of votes received",
+                titlefont: {
+                    color: 'white'
+                },
+                tickfont: {
+                    color: 'white'
+                },
+                linecolor: 'white',
+            }
+        };
+
+        const shopMostChart = [{
+            x: shopMostLabels,
+            y: shopMostValues,
+            type: "bar",
+            orientation: "v",
+            marker: { color: "#4682B4" },
+        }];
+
+        Plotly.newPlot("shop-most-chart", shopMostChart, shopMostLayout, config);
+
+        /// BAR CHART FOR SHOPPING PREFERENCES ////
+        const preferencesData = data.shopmost;
+        const preferencesLabels = ["In person", "Online pickup", "Online delivery", "Other"];
+        var preferencesValues = [];
+        preferencesLabels.map((category) => {
+            preferencesValues.push(preferencesData[category])
+        })
+
+        const preferencesLayout = {
+            title: {
+                text: "Preferred mode of shopping",
+                font: {
+                    color: "white"
+                }
+            },
+            plot_bgcolor: "#282c34",
+            paper_bgcolor: "#282c34",
+            xaxis: {
+                title: "Mode",
+                titlefont: {
+                    color: 'white'
+                },
+                tickfont: {
+                    color: 'white'
+                },
+                linecolor: 'white',
+                zerolinecolor: 'white'
+            },
+            yaxis: {
+                title: "Number of votes received",
+                titlefont: {
+                    color: 'white'
+                },
+                tickfont: {
+                    color: 'white'
+                },
+                linecolor: 'white',
+            }
+        };
+
+        const preferencesChart = [{
+            x: preferencesLabels,
+            y: preferencesValues,
+            type: "bar",
+            orientation: "v",
+            marker: { color: "#4682B4" },
+        }];
+
+        Plotly.newPlot("preferences-chart", preferencesChart, preferencesLayout, config);
+
+
+        /// COMMENTS SECTION ///
+        var comments = data.imp;
+        var commentsList = document.getElementById("comments-list");
+        comments.forEach(function (comment) {
+            var item = document.createElement("li");
+            item.textContent = comment;
+            commentsList.appendChild(item);
+        })
+
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
--- Lab6/server.js
+++ Lab6/server.js
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true })); //parse urls
 app.use(express.static(path.join(__dirname, "public"))); //parse the css and scripts, without this we can't use these from the backend
 app.use(bodyParser.json());
 
+var formData;
+
 /// API ENDPOINTS ///
 
 // Endpoint: to view the survey when accessing from the server
@@ -25,50 +27,46 @@ app.post("/submit", (req, res) => {
   const formData = req.body;
   const filePath = path.join(__dirname, "data", "savedform.json");
   fs.readFile(filePath, 'utf-8')
-  .then(existingData => {
-    let combinedData;
+    .then(existingData => {
+      let combinedData;
 
-    try {
-      // Parse the existing data
-      combinedData = JSON.parse(existingData);
-    } catch (error) {
-      // If parsing fails or the file is empty, initialize with an empty object
-      combinedData = {};
-    }
-
-    for (const entry of Object.entries(formData).slice(2, )) {
-      const [key, value] = entry;
-      if(key === "imp"){
-        combinedData[key].push(value);
-      }else{
-        console.log("bbb " + key + " " + value)
-        if(typeof value === "string") combinedData[key][value] = ++combinedData[key][value];
-        else{
-          console.log("aaa " + key)
-          for(const i of value){
-            console.log(value)
-            combinedData[key][i] = ++combinedData[key][i];
-          }
-        }
-    
+      try {
+        // Parse the existing data
+        combinedData = JSON.parse(existingData);
+      } catch (error) {
+        // If parsing fails or the file is empty, initialize with an empty object
+        combinedData = {};
       }
-    }
 
-    // Convert combinedData to a string
-    const formDataString = JSON.stringify(combinedData, null, 2);
+      for (const entry of Object.entries(formData).slice(2,)) {
+        const [key, value] = entry;
+        if (key === "imp") {
+          combinedData[key].push(value);
+        } else {
+          if (typeof value === "string") combinedData[key][value] = ++combinedData[key][value];
+          else {
+            for (const i of value) {
+              combinedData[key][i] = ++combinedData[key][i];
+            }
+          }
 
-    // Write the updated data back to the file
-    return fs.writeFile(filePath, formDataString);
-  })
-  .then(() => {
-    console.log("Form data appended to file successfully.");
-    // Redirect to the analysis page after saving the data
-    res.redirect("/analysis");
-  })
-  .catch((err) => {
-    console.error("Error writing file:", err);
-    res.status(500).send("Error writing file");
-  });
+        }
+      }
+      // Convert combinedData to a string
+      const formDataString = JSON.stringify(combinedData, null, 2);
+
+      // Write the updated data back to the file
+      return fs.writeFile(filePath, formDataString);
+    })
+    .then(() => {
+      console.log("Form data appended to file successfully.");
+      // Redirect to the analysis page after saving the data
+      res.redirect("/analysis");
+    })
+    .catch((err) => {
+      console.error("Error writing file:", err);
+      res.status(500).send("Error writing file");
+    });
 });
 
 app.get("/analysis", async (req, res) => {
@@ -76,13 +74,15 @@ app.get("/analysis", async (req, res) => {
   try {
     const data = await fs.readFile(filePath, "utf-8");
 
-    console.log("displaying form data on analysis");
+    console.log("Displaying form data on analysis");
 
     const templatePath = path.join(__dirname, "backend", "analysis.html");
     let htmlTemplate = await fs.readFile(templatePath, "utf-8");
 
     const jsonData = JSON.parse(data);
     const imp = jsonData["imp"];
+
+    formData = jsonData;
 
     // call separate functions for everything
     htmlTemplate = replaceCategoryInTemplate(htmlTemplate, jsonData["rating"]);
@@ -100,14 +100,23 @@ app.get("/analysis", async (req, res) => {
   }
 });
 
+// Endpoint to get the json data of the response given by user
+app.get('/api/data', (req, res) => {
+  res.json(formData);
+});
+
 function replaceCategoryInTemplate(template, categoryData) {
   for (const category of Object.keys(categoryData)) {
     template = template.replace(new RegExp(`{{${category}}}`, 'g'), categoryData[category]);
-    console.log(categoryData[category]);
   }
   return template;
 }
 
+
 app.listen(PORT, () =>
   console.log(`Server is running on http://localhost:${PORT}`)
 );
+
+module.exports = {
+  formData
+};
