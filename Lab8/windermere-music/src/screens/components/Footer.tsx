a9a0a42f33e8c3f051dc6e9cd936140520c89f67
Selin Kararmaz <selinkararmaz1@gmail.com>
2024-03-20 11:11:26 -0400

    Change in Footer

--- Lab8/windermere-music/src/screens/components/Footer.tsx
+++ Lab8/windermere-music/src/screens/components/Footer.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import './Footer.css'; 
 
 const Footer = () => {
   return (
-    <footer className="text-center text-dark mt-5" style={{ backgroundColor: "#797674", color:"black" }}>
-      <div className="container pt-3">
-        <h6 className="text-dark" style={{ fontSize: "18px" }}>Connect with us through our socials</h6>
-        <section className="mb-3">
-          <SocialIcon className="m-1" url="https://www.instagram.com/" />
-          <SocialIcon className="m-1" url="https://www.tiktok.com/" />
-          <SocialIcon className="m-1" url="https://www.facebook.com/" />
-          <SocialIcon className="m-1" url="http://linkedin.com/" />
-          <SocialIcon className="m-1" url="https://twitter.com/" />
-
-        </section>
+    <footer className="footer">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6">
+            <p>123 Example Street, City, Country</p>
+          </div>
+          <div className="col-sm-6 text-right">
+            <SocialIcon className="m-1" url="https://www.instagram.com/" />
+            <SocialIcon className="m-1" url="https://www.tiktok.com/" />
+            <SocialIcon className="m-1" url="https://www.facebook.com/" />
+            <SocialIcon className="m-1" url="http://linkedin.com/" />
+            <SocialIcon className="m-1" url="https://twitter.com/" />
+          </div>
+        </div>
       </div>
-      <div className="custom-navbar text-black d-flex align-items-center justify-content-center p-3" >
+      <div className="custom-navbar text-black d-flex align-items-center justify-content-center p-3">
         <span style={{ color: "black" }}>Windermere Music Club © {new Date().getFullYear()}. All Rights Reserved.</span>
       </div>
     </footer>
