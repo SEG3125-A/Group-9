87858c1e22ac78e03582105f8d37998bc3cfc255
Selin Kararmaz <selinkararmaz1@gmail.com>
2024-03-20 12:42:27 -0400

    Change footer display

--- Lab8/windermere-music/src/screens/components/Footer.tsx
+++ Lab8/windermere-music/src/screens/components/Footer.tsx
@@ -6,31 +6,43 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="text-center text-dark mt-5" style={{ backgroundColor: "#797674", color: "black" }}>
-      <div className="container pt-3">
+      <div className="container-fluid">
         <div className="row">
-          <div className="col-md-5 d-flex flex-column justify-content-start align-items-start">
-            <h6 className="text-dark" style={{ fontSize: "18px" }}>Location</h6>
+          <div className="col-md-3 footer-title">
+            <h5>Windermere Music Club</h5>
+          </div>
+          <div className="col-md-3 d-flex flex-column justify-content-start">
             <address>
-               Lincoln Center Plaza<br />
-              New York, NY 10023<br />
-              212-799-5000
+              <h6 className="text-dark" style={{ fontSize: "18px" }}>Location</h6>
+              Lincoln Center Plaza, New York, NY 10023<br />
+              Phone number: 212-799-5000<br />
+              Email: windermereMC@gmail.com
             </address>
-            <section className="mb-3">
-              <FontAwesomeIcon icon={faInstagram} className="m-1 social-media-icon" />
-              <FontAwesomeIcon icon={faTiktok} className="m-1 social-media-icon" size="lg" />
-              <FontAwesomeIcon icon={faFacebook} className="m-1 social-media-icon" size="1x" />
-              <FontAwesomeIcon icon={faLinkedin} className="m-1 social-media-icon" size="lg" />
-              <FontAwesomeIcon icon={faTwitter} className="m-1 social-media-icon" size="lg" />
+          </div>
+          <div className="col-md socials flex-column justify-content-start align-items-start">
+            <h6 className="text-dark" style={{ fontSize: "18px" }}>Socials</h6>
+            <section className="mb-3 socials d-flex align-items-center">
+              <div className="d-flex align-items-center mr-3">
+                <FontAwesomeIcon icon={faInstagram} className="m-1 social-media-icon" />
+                <p className="mb-0 ml-1">Instagram</p>
+              </div>
+              <div className="d-flex align-items-center mr-3">
+                <FontAwesomeIcon icon={faTiktok} className="m-1 social-media-icon" size="lg" />
+                <p className="mb-0 ml-1">Tiktok</p>
+              </div>
+              <div className="d-flex align-items-center mr-3">
+                <FontAwesomeIcon icon={faFacebook} className="m-1 social-media-icon" size="1x" />
+                <p className="mb-0 ml-1">Facebook</p>
+              </div>
+              <div className="d-flex align-items-center mr-">
+                <FontAwesomeIcon icon={faLinkedin} className="m-1 social-media-icon" size="lg" />
+                <p className="mb-0 ml-1">Linkedin</p>
+              </div>
+              <div className="d-flex align-items-center">
+                <FontAwesomeIcon icon={faTwitter} className="m-1 social-media-icon" size="lg" />
+                <p className="mb-0 ml-1">Twitter</p>
+              </div>
             </section>
-          </div>
-          {/* Column for social icons */}
-          <div className="col-md-4">
-            <h6 className="text-dark" style={{ fontSize: "18px" }}>Connect with us through our socials</h6>
-          </div>
-          {/* Column for other information */}
-          <div className="col-md-4">
-            <h6 className="text-dark" style={{ fontSize: "18px" }}>Other Information</h6>
-            <p>Enter other information here</p>
           </div>
         </div>
       </div>
